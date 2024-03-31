0d713277aad0a2a47781fa208443a9e489ad1f07
Arjun <niteshkrjhag@gmail.com>
2024-03-31 19:07:39 +0530

    Parameters are removed from DisplayTime fn & main fn is created

--- Digital_Clock.cpp
+++ Digital_Clock.cpp
@@ -9,15 +9,28 @@ class DigitalC{
     DigitalC(int h,int m,int s):hour(h),min(m),sec(s){
 
     }
-    void Display(int h,int m,int s){
-        if(h>0 && h<=11){
-            if(h==0) h=12;
-            cout<<"Your Time is : "<<h<<": "<<m<<": "<<s<<": "<<"AM";
+    void DisplayTime(){
+        if(hour>0 && hour<=11){
+            if(hour==0) hour=12;
+            cout<<"Your Time is : "<<hour<<": "<<min<<": "<<sec<<": "<<"AM";
         }else{
-            if(h>12) h = h -12;
-            cout<<"Your Time is : "<<h<<": "<<m<<": "<<s<<": "<<"AM";
+            if(hour>12) hour = hour -12;
+            cout<<"Your Time is : "<<hour<<": "<<min<<": "<<sec<<": "<<"AM";
         }
     }
 
 };
 
+int main(){
+    int hours,minutes,seconds;
+    cout<<"Enter Hours : ";
+    cin>>hours;
+    cout<<"Enter Minutes : ";
+    cin>>minutes;
+    cout<<"Enter Seconds : ";
+    cin>>seconds;
+
+DigitalC digital = DigitalC(hours,minutes,seconds);
+digital.DisplayTime();
+
+}
